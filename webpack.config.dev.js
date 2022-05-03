5c4ba17349c1b3e4c3b68ab66475ef18d5e70554
Siarhei Samets <siarhei_samets@epam.com>
2022-05-03 10:22:05 +0300

    dev version

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.join(__dirname, '/dist'),
         filename: "index_bundle.js"
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
